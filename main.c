1d215ec310aed4652ba84365e2523c2473d00ef4
Martin Wiulsrød <wiu753@gmail.com>
2022-04-06 19:13:06 +0200

    Remove deposit and withdraw for a single transaction function

--- main.c
+++ main.c
@@ -4,11 +4,19 @@
 #include <string.h>
 #include <unistd.h>
 
-void *deposit(void *arg);
-void *withdraw(void *arg);
+void *transaction(void *arg);
 int initializeMutex(pthread_mutex_t *mutex);
 int createThreads(pthread_t *threads, int numThreads, pthread_mutex_t *mutex);
 void joinThreads(pthread_t *threads, int numThreads);
+
+#define DEPOSIT 0
+#define WITHDRAW 1
+
+struct args
+{
+    pthread_mutex_t *mutex;
+    int threadId;
+};
 
 int balance;
 
@@ -44,16 +52,47 @@ int createThreads(pthread_t *threads, int numThreads, pthread_mutex_t *mutex)
 
     while (i < numThreads)
     {
-        if (i == 0)
-            error = pthread_create(&threads[i], NULL, deposit, mutex);
-        else
-            error = pthread_create(&(threads[i]), NULL, &withdraw, mutex);
+        struct args *TransactionArgs = (struct args *)malloc(sizeof(struct args));
+        TransactionArgs->mutex = mutex;
+        TransactionArgs->threadId = i;
+
+        error = pthread_create(&(threads[i]), NULL, transaction, (void *)TransactionArgs); // Have to pass in I and mutex
 
         if (error != 0)
             printf("\nThread can't be created :[%s]", strerror(error));
         i++;
     }
     return 0;
+}
+
+void *transaction(void *arg)
+{
+    FILE *ptr;
+    char str[10];
+
+    if (((struct args *)arg)->threadId == 0)
+    {
+        ptr = fopen("inn.txt", "r");
+
+        if (NULL == ptr)
+            printf("File can't be opened \n");
+    }
+    else if (((struct args *)arg)->threadId == 1)
+        ptr = fopen("ut.txt", "r");
+    else
+        printf("Error: invalid thread id");
+
+    if (NULL == ptr)
+        printf("File can't be opened \n");
+
+    while (fgets(str, sizeof(str), ptr) != NULL)
+    {
+        pthread_mutex_lock(((struct args *)arg)->mutex);
+        (((struct args *)arg)->threadId) ? (balance -= atoi(str)) : (balance += atoi(str));
+        pthread_mutex_unlock(((struct args *)arg)->mutex);
+    }
+
+    return NULL;
 }
 
 void joinThreads(pthread_t *threads, int numThreads)
@@ -65,44 +104,4 @@ void joinThreads(pthread_t *threads, int numThreads)
         pthread_join(threads[i], NULL);
         i++;
     }
-}
-
-void *deposit(void *arg)
-{
-    FILE *ptr;
-    char str[50];
-    ptr = fopen("inn.txt", "r");
-
-    if (NULL == ptr)
-        printf("File can't be opened \n");
-
-    while (fgets(str, 50, ptr) != NULL)
-    {
-        pthread_mutex_lock(arg);
-        balance += atoi(str);
-        pthread_mutex_unlock(arg);
-    }
-
-    fclose(ptr);
-    return NULL;
-}
-
-void *withdraw(void *arg)
-{
-    FILE *ptr;
-    char str[50];
-    ptr = fopen("ut.txt", "r");
-
-    if (NULL == ptr)
-        printf("File can't be opened\n");
-
-    while (fgets(str, 50, ptr) != NULL)
-    {
-        pthread_mutex_lock(arg);
-        balance -= atoi(str);
-        pthread_mutex_unlock(arg);
-    }
-
-    fclose(ptr);
-    return NULL;
 }
