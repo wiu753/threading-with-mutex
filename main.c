d9d61717a0e0dbf6f479ae7516882f84a4569876
Martin Wiulsrød <wiu753@gmail.com>
2022-04-06 19:28:52 +0200

    Update error handling and add comments

--- main.c
+++ main.c
@@ -6,11 +6,8 @@
 
 void *transaction(void *arg);
 int initializeMutex(pthread_mutex_t *mutex);
-int createThreads(pthread_t *threads, int numThreads, pthread_mutex_t *mutex);
-void joinThreads(pthread_t *threads, int numThreads);
-
-#define DEPOSIT 0
-#define WITHDRAW 1
+int runTransactions(pthread_t *threads, int numThreads, pthread_mutex_t *mutex);
+void closeThreads(pthread_t *threads, int numThreads, pthread_mutex_t *mutex);
 
 struct args
 {
@@ -25,10 +22,13 @@ int main(void)
     pthread_mutex_t lock;
     pthread_t threadId[2];
 
-    initializeMutex(&lock);
-    createThreads(threadId, 2, &lock);
-    joinThreads(threadId, 2);
-    pthread_mutex_destroy(&lock);
+    if (initializeMutex(&lock))
+        printf("Error initializing mutex\n");
+
+    if (runTransactions(threadId, 2, &lock) != 0) // The threads also run the transaction function
+        printf("Error running transactions\n");
+
+    closeThreads(threadId, 2, &lock);
 
     printf("Balance: %d", balance);
 
@@ -45,21 +45,23 @@ int initializeMutex(pthread_mutex_t *mutex)
     return 0;
 }
 
-int createThreads(pthread_t *threads, int numThreads, pthread_mutex_t *mutex)
+int runTransactions(pthread_t *threads, int numThreads, pthread_mutex_t *mutex)
 {
     int i = 0;
-    int error;
 
     while (i < numThreads)
     {
+        // To pass multiple arguments to pthread_create you need to use a struct
         struct args *TransactionArgs = (struct args *)malloc(sizeof(struct args));
         TransactionArgs->mutex = mutex;
         TransactionArgs->threadId = i;
 
-        error = pthread_create(&(threads[i]), NULL, transaction, (void *)TransactionArgs); // Have to pass in I and mutex
+        if (pthread_create(&(threads[i]), NULL, transaction, (void *)TransactionArgs) != 0)
+        {
+            printf("\nThread creation failed\n");
+            return 1;
+        }
 
-        if (error != 0)
-            printf("\nThread can't be created :[%s]", strerror(error));
         i++;
     }
     return 0;
@@ -71,12 +73,7 @@ void *transaction(void *arg)
     char str[10];
 
     if (((struct args *)arg)->threadId == 0)
-    {
         ptr = fopen("inn.txt", "r");
-
-        if (NULL == ptr)
-            printf("File can't be opened \n");
-    }
     else if (((struct args *)arg)->threadId == 1)
         ptr = fopen("ut.txt", "r");
     else
@@ -85,6 +82,7 @@ void *transaction(void *arg)
     if (NULL == ptr)
         printf("File can't be opened \n");
 
+    // If the thread id is 1 (true) then it is a withdraw, otherwise it is a deposit
     while (fgets(str, sizeof(str), ptr) != NULL)
     {
         pthread_mutex_lock(((struct args *)arg)->mutex);
@@ -95,7 +93,7 @@ void *transaction(void *arg)
     return NULL;
 }
 
-void joinThreads(pthread_t *threads, int numThreads)
+void closeThreads(pthread_t *threads, int numThreads, pthread_mutex_t *mutex)
 {
     int i = 0;
 
@@ -104,4 +102,5 @@ void joinThreads(pthread_t *threads, int numThreads)
         pthread_join(threads[i], NULL);
         i++;
     }
+    pthread_mutex_destroy(mutex);
 }
